cc7d950b4aaf078bf1bd274d7bb3397fb38b4861
emanuel2718 <eramirez2718@gmail.com>
2025-11-09 15:18:36 -0400

    chore: bump version

--- flake.nix
+++ flake.nix
@@ -26,7 +26,7 @@
 
         packages.default = pkgs.rustPlatform.buildRustPackage {
           pname = "termitype";
-          version = "0.0.10";
+          version = "0.0.11";
           src = ./.;
           cargoLock.lockFile = ./Cargo.lock;
         };
